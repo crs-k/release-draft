cd2eb7b2865549f2c69f0950dd1a80e4d94d3221
Chris <26232872+crs-k@users.noreply.github.com>
2022-01-12 15:29:45 -0500

    changed confg

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
-module.exports = {
-  clearMocks: true,
-  moduleFileExtensions: ['js', 'ts'],
-  testMatch: ['**/*.test.ts'],
-  transform: {
-    '^.+\\.ts$': 'ts-jest'
-  },
-  verbose: true
-}
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sync object
+const config = {
+    verbose: true,
+    clearMocks: true,
+    testMatch: ['**/*.test.ts'],
+    transform: {
+        '^.+\\.ts?$': 'ts-jest',
+    },
+};
+exports.default = config;
