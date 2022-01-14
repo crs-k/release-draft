61ba9e6a078e07af7e58c7106f46770ffb28c75a
Chris <26232872+crs-k@users.noreply.github.com>
2022-01-14 14:47:21 -0500

    formatting

--- src/next-semver.ts
+++ src/next-semver.ts
@@ -1,6 +1,5 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
-
 import * as fs from 'fs'
 import * as semver from 'semver'
 import {context, getOctokit} from '@actions/github'
